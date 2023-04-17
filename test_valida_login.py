4760486bb0941d1527375ec30a954584f6df129b
FELIPE DE AVILA ROOS <89357173+felipeavila97@users.noreply.github.com>
2023-04-17 19:45:48 -0300

    olokinho meu

--- test_valida_login.py
+++ test_valida_login.py
@@ -1,6 +1,7 @@
 from valida_login import *
 
-assert validar_usuario("jokas") == False
-assert validar_usuario("Jokas") == True
-assert validar_senha("123") == False
-assert validar_senha("Senh@do0201") == True
+def test_valida():
+    assert validar_usuario("jokas") == False
+    assert validar_usuario("Jokas") == True
+    assert validar_senha("123") == False
+    assert validar_senha("Senh@do0201") == True
